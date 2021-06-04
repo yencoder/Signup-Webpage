51669edaa84a0f2588499d93138631007495992c
Devashish Kapoor <devashishkpr@gmail.com>
2021-06-04 10:04:02 -0400

    added basic structure for metadata, header, footer

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+</main>
+<footer>
+  <span>&copy; 2021 - COIS 3420 Web Applications</span>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    <h1>Website Name</h1>
+</header>
+
+<main>
+    <nav>
+        
+    </nav>
