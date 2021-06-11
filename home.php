0b5a3b1dc3adb0d6f31e907e1d7ee14c90f734e4
Devashish Kapoor <devashishkpr@gmail.com>
2021-06-11 12:36:54 -0400

    semi-styled

--- home.php
+++ home.php
@@ -1,9 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <?php $page_title = "Collab 101"; ?>
+    <?php include "includes/metadata.php" ?>
   </head>
   <body>
    <?php include 'includes/header.php';?>
-      <section></section>
-  </body> 
-  <footer></footer>
+    <!-- Sign up content -->
+    <section>
+      <h2>Group Organizing Made Easy</h2>
+      <button>Sign up</button>
+      <i class="fas fa-pencil-alt"></i>
+    </section>
+    <!-- What we Offer -->
+    <section>
+      <h2>What we Offer</h2>
+    </section>
+    <!-- About us -->
+    <section>
+      <h2>Meet the Creators</h2>
+      <p>
+        Hi&#33; I&#39;m Fanshu. I&#39;m a Computer Science and Business student. 
+        I love fitness and muscle building. Coding is what motivated me to think logically.
+      </p>
+      <p>
+        Hi, my name is Eesha. I&#39;m 19 years old student studying Computer Science at Trent University. 
+        I enjoy reading&comma; painting and swimming.
+      </p>
+      <p>
+        Hi there&#33; I&#39;m Devashish. I&#39;m a Computer Science student specializing in Software Engineering.
+        You can often find me tossing a frisbee around my neighbourhood and sometimes even on long bike rides.
+      </p>
+    </section>
+    <?php include "includes/footer.php" ?>
+  </body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,11 @@
 <header>
-  <img src="" alt="" />
   <h1>Collab with Us</h1>
-  <img src="" alt="" />
-</header>
-
-<main>
   <nav>
     <ul>
       <li><a href="home.php">Home</a></li>
       <li><a href="signup.html">Sign Up</a></li>
       <li><a href="login.html">Login</a></li>
-      <li><a href="guestsheet.html">Try Signup Sheet</a></li>
     </ul>
   </nav>
+</header>
+<main>
