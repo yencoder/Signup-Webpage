06213a896e11e4ba894004323cc126acd36d5eb9
mengfanshu <84322803+mengfanshu@users.noreply.github.com>
2021-06-15 15:11:34 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
   <nav>
     <ul>
       <li><a href="home.php">Home</a></li>
-      <li><a href="signup.html">Sign Up</a></li>
+      <li><a href="signup.php">Sign Up</a></li>
       <li><a href="login.php">Login</a></li>
     </ul>
   </nav>
