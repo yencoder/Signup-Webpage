c16537cc649138a93542e24bd2bd215dd21d46eb
mengfanshu <84322803+mengfanshu@users.noreply.github.com>
2021-06-26 15:26:09 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,3 +1,36 @@
+<?php
+
+$errors = array(); //declare empty array to add errors too
+
+//get name from post or set to NULL if doesn't exist
+$title = $_POST['title'] ?? null;
+$description = $_POST['description'] ?? null;
+$privacy = $_POST['status'] ?? null;
+
+
+
+if (isset($_POST['save'])) { 
+
+    //validate user has entered a name
+    if (!isset($title) || strlen($title) === 0) {
+        $errors['title'] = true;
+    }
+    //validate user has entered a correct gps coordinate
+    if (!isset($description) || strlen($description) === 0) {
+      $errors['description'] = true;
+  }
+
+    if (empty($privacy)) {
+    $errors['privacy'] = true;
+}
+      //saved page
+   if(count($errors)=== 0){
+      header("Location: home.php");
+         exit;
+    }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,39 +38,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/6786f5cbb4.js" crossorigin="anonymous"></script>
     <title>Sign Up Sheet</title>
-    <link rel="stylesheet" href="styles/signup.css" />
+    <link rel="stylesheet" href="styles/master.css" />
   </head>
   <body>
     <?php include 'includes/header.php';?>
-          <form>
+    <section class="signup">
+          <form id="signupform" action="<?=htmlentities($_SERVER['PHP_SELF']);?>" method="post" novalidate>
           <h2>
               Create Your Sign Up Sheet
           </h2>
             <div class="input">
                 <label for="title">Sheet Title</label>
-                <input id="title" name="title" type="text" placeholder="COIS 3420 Project" />
+                <input id="title" name="title" type="text" placeholder="COIS 3420 Project" value="<?=$title?>"/>
+                <span class="error <?=!isset($errors['title']) ? 'hidden' : "";?>">Please enter a Sheet Title</span>
               </div>
               <div class="input">
                 <label for="description">Description</label>
-                <textarea name="description" id="description" cols="50" rows="5"></textarea>
+                <textarea name="description" id="description" cols="50" rows="5" value="<?=$description?>"></textarea>
+                <span class="error <?=!isset($errors['description']) ? 'hidden' : "";?>">Please Write a Description</span>
               </div>
-              <div class="input"> <!-- add multiple time slots for x users -->
+              <div class="input">
+                <label for="member">Members</label>
+                <input id="name" name="member" type="text" placeholder="Member's Name" >
+                <input id="email" name="member" type="text" placeholder="Member's Email Address" >
+                <button id="add" name="add">+add</button>
+              </div>
+              <div class="input">
                 <label for="date">Time Slot</label>
                 <input id="date" name="date" type="datetime-local" />
+                <button id="add" name="add">+add</button>
               </div>
               <fieldset>
                 <legend>Privacy</legend>    
                 <div>
-                  <input id="public" name="privacy" type="radio" value="Y" />
+                  <input id="public" name="status" type="radio" value="Y" <?=$privacy == "Y" ? 'checked' : ''?> />
                   <label for="public">Public</label>
                 </div>
                 <div>
-                  <input id="private" name="privacy" type="radio" value="N" />
+                  <input id="private" name="status" type="radio" value="N" <?=$privacy == "N" ? 'checked' : ''?> />
                   <label for="private">Private</label>
-                </div>
+                </div>          
               </fieldset>
-              <button id="save">Save</button>
+              <span class="error <?=!isset($errors['privacy']) ? 'hidden' : "";?>">Please choose one</span>
+              <button id="save" name='save'>Save</button>
           </form>
+  </section>
     <?php include "includes/footer.php" ?>
   </body>
 </html>
