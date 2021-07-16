37c67532f066982287f6295fc978e5d0827f3624
mengfanshu <84322803+mengfanshu@users.noreply.github.com>
2021-07-16 21:44:52 +0800

    Update search.php

--- search.php
+++ search.php
@@ -5,15 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/6786f5cbb4.js" crossorigin="anonymous"></script>
     <title>Search Page</title>
-    <link rel="stylesheet" href="styles/search.css" />
+    <link rel="stylesheet" href="styles/master.css" />
   </head>
   <body>
     <?php include 'includes/header.php';?>
+    <form class="searchform">
     <h2>Search Your Sign Up Sheet</h2>
-    <section>
+    <section class = "search">
     <i class="fa fa-search"></i>
-    <input id="search" name="search" type="text" placeholder="Enter Search Title, Username, Time Slot..." />
+    <input id="search" name="search" type="text" placeholder="Enter title, username, time slot..." />
     <button id="go">Search</button>
 </section>
+</form>
     <?php include "includes/footer.php" ?>
 </body>
