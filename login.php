1a3ab944526b3fefdcffccc2c3e7d99673d2d70c
eeshadawoodia <77733503+eeshadawoodia@users.noreply.github.com>
2021-07-23 21:24:53 +0300

    made it point to mystuffview

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if (isset($_POST['submit'])) { //only do this code if the form has been submitte
       $_SESSION['username'] = $user;
       $_SESSION['userid'] = $row['userid'];
 
-      header("Location:home.php");
+      header("Location:mystuffview.php");
       exit();
 
     } else {
@@ -73,7 +73,7 @@ if (isset($_POST['submit'])) { //only do this code if the form has been submitte
         <input type="checkbox" name="remember">
         </div>
         <div>
-        <span class="psw"><a href="forgot.php">Forgot password</a></span>
+        <span class="psw"><a href="#">Forgot password</a></span>
         <span><a href="create.php">New account</a></span>
         </div>
       </div>    
